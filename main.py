2ea275933f4a3b6a76c11e45af29635b628e4624
LiLatee <30510627+LiLatee@users.noreply.github.com>
2018-03-18 12:54:23 +0100

    bugs a

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from random import randrange
+import time
 import os
 
 # TODO: 1. Czy zrobione czy już było ?
 # TODO: 2. FileNotFoundError
-# TODO: 3. Wyłączenie gdy otwieramy zakończony plik.
+# TODO: 2. Powrót
 
 def get_range(range: list) -> int:
     try:
@@ -14,32 +15,58 @@ def get_range(range: list) -> int:
     range.append(number1)
     range.append(number2)
     return 0
-def open_write(filename: str=None) -> str:
+def open_write( action: str, filename: str=None) -> str:
     """Opens existing file and saves data."""
 
-    if filename is None:
-        filename = input('Podaj nazwe pliku do otwarcia: ')
-    set_of_elements = set()
+    #if filename is None:
+    # err = FileNotFoundError
+    #
+    # while err is FileNotFoundError:
+    #     try:
+    #         err = None
+    #         filename = input('Podaj nazwe pliku do otwarcia: ')
+    #
+    #         set_of_elements = set()
+    #         with open(filename, 'r') as data:
+    #             number1, number2 = data.readline().split(' ')
+    #             # Removes square brackets
+    #             for element in data:
+    #                 # Creates set with elements from a file, converts strings to int.
+    #                 set_of_elements = set(map(int, (element.strip('{}\n')).split(', ')))
+    #     except FileNotFoundError:
+    #         err = FileNotFoundError
 
-    with open(filename, 'r') as data:
-        number1, number2 = data.readline().split(' ')
-        # Removes square brackets
-        for element in data:
-            # Creates set with elements from a file, converts strings to int.
-            set_of_elements = set(map(int, (element.strip('{}\n')).split(', ')))
+
+    try:
+        if filename is None:
+            filename = input('Podaj nazwe pliku do otwarcia: ')
+
+        set_of_elements = set()
+        with open(filename, 'r') as data:
+            number1, number2 = data.readline().split(' ')
+            # Removes square brackets
+            for element in data:
+                # Creates set with elements from a file, converts strings to int.
+                set_of_elements = set(map(int, (element.strip('{}\n')).split(', ')))
+    except FileNotFoundError:
+        print('Plik nie istnieje!')
+        time.sleep(2)
+        return FileNotFoundError
 
     draw_number = randrange(int(number1), int(number2) + 1, 1)
 
     done_exercises = 0
+    list_of_elements = []
     while len(set_of_elements) != (int(number2) - int(number1) + 1):
         if draw_number not in set_of_elements:
             os.system('cls')
             print('Wylosowano: ', draw_number)
             set_of_elements.add(draw_number)
-            print(set_of_elements)
+            list_of_elements.append(draw_number)
             print('Zadanie numer: ', len(set_of_elements), '/', (int(number2)-int(number1) + 1), '.')
             done_exercises += 1
             print('Aktualnie robione zadanie: ', done_exercises, '.')
+            print(list_of_elements) # Shows currently performed exercises during this session.
             action = input('Wciśnij cokolwiek, aby wylosowac kolejną liczbę lub Q, aby zakończyć: ')
         draw_number = randrange(int(number1), int(number2) + 1, 1)
         if action.upper() == 'Q':
@@ -54,7 +81,6 @@ def open_write(filename: str=None) -> str:
         return action
     else:
         print('Wszystkie zadania zostały zrobione! GRATULACJE! :)')
-        action = input('Stworzyć nowy zakres (N), czy wczytac ostatni (O)? ')
         return action
 def create_write_file() -> str:
     """Creates files if doesn't exist and uses open_write() function."""
@@ -80,7 +106,7 @@ def create_write_file() -> str:
     with open(name, 'w') as data:
         print(number1, number2, file=data, end='\n')
 
-    return open_write(name)
+    return open_write(action, name)
 
 
 # Creates a folder for saves if doesn't exist.
@@ -95,6 +121,6 @@ while action.upper() != 'Q':
     if action.upper() == 'N':
         action = create_write_file()
     elif action.upper() == 'O':
-        action = open_write()
-    else:
-        action = input('Wciśnięto zły klawisz. Podaj N lub O (Q aby wyjść): ')
+        action = open_write(action)
+    os.system('cls')
+    action = input('Stworzyć nowy zakres (N), czy wczytać ostatni (O) lub Q aby wyjść: ')
